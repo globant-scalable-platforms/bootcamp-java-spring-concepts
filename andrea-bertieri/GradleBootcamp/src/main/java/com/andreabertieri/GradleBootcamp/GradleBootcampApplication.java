01a5b33605511b24bfb03594032ccb95e0047de2
Andrea <abertierir@unal.edu.co>
2019-05-29 06:40:30 -0500

    First .jar Gradle Spring Andrea Bertieri

---
+++ andrea-bertieri/GradleBootcamp/src/main/java/com/andreabertieri/GradleBootcamp/GradleBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.andreabertieri.GradleBootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleBootcampApplication.class, args);
+	}
+
+}
---
+++ andrea-bertieri/GradleBootcamp/src/test/java/com/andreabertieri/GradleBootcamp/GradleBootcampApplicationTests.java
@@ -0,0 +1,16 @@
+package com.andreabertieri.GradleBootcamp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GradleBootcampApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
