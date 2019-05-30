2c93a664b4bfec6a6eb79234e46c5f1847a9610f
Andrea <abertierir@unal.edu.co>
2019-05-30 16:56:06 -0500

    Find and Remove an Element Facing. HashMap vs TreeMap

--- andrea-bertieri/Week 1/KATA_3/ArrayList vs LinkedList/src/com/bootcamp/HashMapvsTreeMap/HashMapvsTreeMap.java
+++ andrea-bertieri/Week 1/KATA_3/ArrayList vs LinkedList/src/com/bootcamp/HashMapvsTreeMap/HashMapvsTreeMap.java
@@ -6,33 +6,57 @@ import java.util.stream.Collectors;
 public class HashMapvsTreeMap {
 
 	public static void main(String[] args) {
-		Map<Integer, String> map = new HashMap<Integer, String>();
-		Map<Integer, String> treeMap = new TreeMap<Integer, String>();
+		Map<Integer, String> map = populate(new HashMap<Integer, String>());
+		Map<Integer, String> treeMap = populate(new TreeMap<Integer, String>());
 		long start;
 		long end;
-		int remover= 5000;
 		
 		System.out.println("WELLCOME: Today a Map and a treeMap will be faced\nWhich will the winner be?");
 		
 		//Putting a new element
 		System.out.println("\nPUTTING A NEW ELEMENT");
 		start= System.nanoTime();
-		map.put(4,"four");
+		map.put(55,"Cat");
 		end = System.nanoTime();
 
-	    System.out.println("Map's time (Miliseconds): " + ((end - start)));
+	    System.out.println("treeMap's time (Miliseconds): " + ((end - start)));
 	    
 	    start= System.nanoTime();
-	    treeMap.put(4,"four");
+	    treeMap.put(55,"Cat");
 		end = System.nanoTime();
 
 	    System.out.println("HashMap's time (Miliseconds): " + ((end - start)));
-	    System.out.println("The winner is HashMap");
+	    System.out.println("The winner is random. There is no a winner");
 		
 		//Find an element by Key
+	    System.out.println("\nFINDING AN ELEMENT BY KEY");
+		start= System.nanoTime();
+		map.put(1,"January");
+		end = System.nanoTime();
 
+	    System.out.println("treeMap's time (Miliseconds): " + ((end - start)));
+	    
+	    start= System.nanoTime();
+	    treeMap.put(1,"Jamuary");
+		end = System.nanoTime();
+
+	    System.out.println("HashMap's time (Miliseconds): " + ((end - start)));
+	    System.out.println("The winner is TreeMap");
 		
 		//Remove an element by key
+	    System.out.println("\nREMOVING AN ELEMENT BY KEY");
+		start= System.nanoTime();
+		map.remove(1);
+		end = System.nanoTime();
+
+	    System.out.println("treeMap's time (Miliseconds): " + ((end - start)));
+	    
+	    start= System.nanoTime();
+	    treeMap.remove(1);
+		end = System.nanoTime();
+
+	    System.out.println("HashMap's time (Miliseconds): " + ((end - start)));
+	    System.out.println("The winner is TreeMap");
 
 	    
 		//Iterate over the whole entries
@@ -40,12 +64,22 @@ public class HashMapvsTreeMap {
 
 	}
 	
-	private static List<Integer> populate(List<Integer> objects, int amount) {
-        return new Random()
-                .ints()
-                .limit(amount)
-                .boxed()
-                .collect(Collectors.toCollection(() -> objects));
- }
+	 private static Map<Integer, String> populate(Map<Integer, String> objects ){
+		 objects.put(1,"January");
+		 objects.put(2,"Frebruary");
+		 objects.put(3,"March");
+		 objects.put(4,"April");
+		 objects.put(5,"May");
+		 objects.put(6,"June");
+		 objects.put(7,"July");
+		 objects.put(8,"August");
+		 objects.put(9,"September");
+		 objects.put(10,"October");
+		 objects.put(11,"November");
+		 objects.put(12,"December");
+		 
+	        return objects ;
+	 }
+	
 
 }
