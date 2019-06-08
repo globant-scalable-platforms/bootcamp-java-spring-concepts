b9030a7be61515efd23f42f3cba892aec3bf9b14
Luis <luis.castellanos.oviedo@gmail.com>
2019-06-08 09:27:27 -0500

    remove useless import

--- aspects-actuator/src/main/java/com/bootcamp/aopactuator/aspectsactuator/AspectsActuatorApplication.java
+++ aspects-actuator/src/main/java/com/bootcamp/aopactuator/aspectsactuator/AspectsActuatorApplication.java
@@ -2,9 +2,6 @@ package com.bootcamp.aopactuator.aspectsactuator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 public class AspectsActuatorApplication {
