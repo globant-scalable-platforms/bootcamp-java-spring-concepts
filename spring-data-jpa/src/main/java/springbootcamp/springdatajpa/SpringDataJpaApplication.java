50b28fcf3ecb4cf13cbc04bc590235099f9c2065
Daniel Florez Jaramillo <danyflorez1@gmail.com>
2019-06-18 07:55:06 -0500

    enable jpa repositories

--- spring-data-jpa/src/main/java/springbootcamp/springdatajpa/SpringDataJpaApplication.java
+++ spring-data-jpa/src/main/java/springbootcamp/springdatajpa/SpringDataJpaApplication.java
@@ -2,8 +2,10 @@ package springbootcamp.springdatajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class SpringDataJpaApplication {
 
 	public static void main(String[] args) {
